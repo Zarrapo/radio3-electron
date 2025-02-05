54298065faa5a389a10666c9ce3700d0a10117a6
German Sanchez <31968352+Zarrapo@users.noreply.github.com>
2025-02-05 20:13:45 +0100

    Updates

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain, Menu, Tray, shell } from 'electron';
+import { app, BrowserWindow, ipcMain, Menu, Tray } from 'electron';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import Store from 'electron-store';
@@ -9,17 +9,14 @@ const __dirname = path.dirname(__filename);
 const store = new Store();
 let mainWindow;
 let tray;
-let aboutWindow = null; // Nueva variable global para la ventana "Acerca de"
 let isFirstUserAction = true;
-let isModalOpen = false; // Nueva variable para controlar la ventana modal
 
 app.whenReady().then(() => {
     createTray();
     createWindow();
     
     app.setLoginItemSettings({
-        openAtLogin: true,
-        openAsHidden: true,
+        openAtLogin: true
     });
 });
 
@@ -27,26 +24,19 @@ function createTray() {
     tray = new Tray(path.join(__dirname, 'assets', 'icons', 'rne.ico'));
     tray.setToolTip('Reproductor de Radio');
     
-    // Actualización del menú contextual del tray con "Acerca de" al final separado por un separador
     const contextMenu = Menu.buildFromTemplate([
         { label: 'Mostrar/Ocultar', click: () => {
-            if (mainWindow.isVisible()) { mainWindow.hide(); } 
-            else { mainWindow.show(); playLastStation(); }
+            if (mainWindow.isVisible()) {
+                mainWindow.hide();
+            } else {
+                mainWindow.show();
+                playLastStation();
+            }
         }},
-        { label: 'Mover', click: () => { mainWindow.webContents.send('start-window-move'); } },
         { label: 'Salir', click: () => {
-            if(aboutWindow){ 
-                aboutWindow.removeAllListeners('close');
-                aboutWindow.destroy();
-            }
-            if(mainWindow){
-                mainWindow.removeAllListeners('close');
-                mainWindow.destroy();
-            }
+            mainWindow.destroy();
             app.quit();
-        }},
-        { type: 'separator' },
-        { label: 'Acerca de', click: () => { createAboutWindow(); } }
+        }}
     ]);
     
     tray.setContextMenu(contextMenu);
@@ -82,28 +72,24 @@ function createWindow() {
         resizable: false // Permite redimensionar
     });
 
-    // Cargar el archivo HTML de la ventana principal
     mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
     // Eliminar menú de Electron
     Menu.setApplicationMenu(null);
 
-    // Actualización del menú contextual para la ventana: se separa "Acerca de" con un separador
     const contextMenu = Menu.buildFromTemplate([
         { label: 'Mostrar/Ocultar', click: () => {
-            if (mainWindow.isVisible()) { mainWindow.hide(); } 
-            else { mainWindow.show(); playLastStation(); }
-        }},
-        { label: 'Mover', click: () => { mainWindow.webContents.send('start-window-move'); } },
-        { label: 'Salir', click: () => {
-            if(mainWindow){
-                mainWindow.removeAllListeners('close'); // Remover listeners para evitar error
-                mainWindow.destroy();
+            if (mainWindow.isVisible()) {
+                mainWindow.hide();
+            } else {
+                mainWindow.show();
+                playLastStation();
             }
-            app.quit();
         }},
-        { type: 'separator' },
-        { label: 'Acerca de', click: () => { createAboutWindow(); } }
+        { label: 'Salir', click: () => {
+            mainWindow.destroy();
+            app.quit();
+        }}
     ]);
 
     mainWindow.webContents.once('did-finish-load', () => {
@@ -124,29 +110,10 @@ function createWindow() {
         store.set('windowState', mainWindow.getBounds()); // Guarda posición
     });
 
-    mainWindow.on('blur', () => {
-        if (!isModalOpen) {
-            mainWindow.hide();
-        }
-    });
-
     mainWindow.webContents.on('context-menu', () => {
         contextMenu.popup();
     });
 }
-
-// IPC para obtener la posición actual de la ventana
-ipcMain.handle('get-window-position', () => {
-    return mainWindow.getPosition();
-});
-
-// IPC para actualizar la posición de la ventana
-ipcMain.on('update-window-position', (event, pos) => {
-    mainWindow.setPosition(pos.x, pos.y);
-    // Guardar la nueva posición
-    const bounds = mainWindow.getBounds();
-    store.set('windowState', bounds);
-});
 
 function playLastStation() {
     const lastStation = store.get('lastStation');
@@ -163,40 +130,3 @@ ipcMain.on('change-station', (event, url) => {
         isFirstUserAction = false;
     }
 });
-
-function createAboutWindow() {
-    if (!mainWindow.isVisible()) {
-        mainWindow.show(); // Mostrar la aplicación si está oculta
-    }
-    isModalOpen = true; // Indicar que se abre la ventana modal
-    aboutWindow = new BrowserWindow({
-        width: 400,
-        height: 400,
-        resizable: false,
-        minimizable: false,
-        maximizable: false,
-        parent: mainWindow,
-        modal: true,
-        title: 'Acerca de',
-        icon: path.join(__dirname, 'assets', 'icons', 'Info.ico'), // Nuevo icono
-        webPreferences: { 
-            nodeIntegration: true,
-            contextIsolation: false 
-        }
-    });
-    aboutWindow.setMenu(null);
-    aboutWindow.loadFile(path.join(__dirname, 'about.html'));
-    // Al cerrarse, se limpia la variable y se vuelve a situar el foco sobre la aplicación
-    aboutWindow.on('closed', () => {
-        isModalOpen = false; // Restablecer bandera de modal cerrado
-        aboutWindow = null; // Limpiar la referencia
-        mainWindow.show();
-        mainWindow.focus();
-    });
-
-    // Configuramos el handler para abrir enlaces externamente
-    aboutWindow.webContents.setWindowOpenHandler(({ url }) => {
-        shell.openExternal(url);
-        return { action: 'deny' };
-    });
-}
